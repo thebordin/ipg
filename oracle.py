8bd7bad8a7d98eb691d5c5eb20265bb6b5e4e02c
thebordin <115535431+thebordin@users.noreply.github.com>
2023-05-27 16:38:09 +0100

    vd

--- oracle.py
+++ oracle.py
@@ -1,5 +1,5 @@
-import
-
+import oracledb
+#cx_Oracle.init_oracle_client(lib_dir="/instantclient_21_10")
 # Configurações de conexão
 host = 'bdi.ipg.pt'
 port = '1521'
@@ -8,8 +8,8 @@ username = 'bdi_dai_1707750'
 password = 'bdi'
 
 # Cria a string de conexão
-dsn = cx_Oracle.makedsn(host, port, service_name=service_name)
-connection = cx_Oracle.connect(user=username, password=password, dsn=dsn)
+dsn = oracledb.makedsn(host, port)
+connection = oracledb.connect(user=username, password=password, dsn=dsn)
 
 try:
     # Cria um cursor
